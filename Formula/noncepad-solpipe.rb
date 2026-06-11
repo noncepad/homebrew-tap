4ae038ad2031d3aa97e66fbda44351e1fa52411a
Joel De Jesus <dejesus.joel@e-flamingo.jp>
2026-06-11 12:01:53 +0900

    update

--- Formula/noncepad-solpipe.rb
+++ Formula/noncepad-solpipe.rb
@@ -1,18 +1,18 @@
 class NoncepadSolpipe < Formula
   desc "Solpipe and Safejar Command Line Utilities"
   homepage "https://solpipe.io"
-  version "0.4.966"
+  version "0.4.970"
   license "PRIVATE"
 
 
   on_macos do
     on_intel do
       url "https://noncepad.com/dev/nightly/macos/intel/solpipe.tar.gz"
-      sha256 "ca620ddb1f2623b72f006299e9651a000c167dc3df6c518440f72dbe10df1427"
+      sha256 "089ac41db6406f7b928c66d5f58c991f3f608bbb0e938bf55a396f6ea2c5be85"
     end
     on_arm do
       url "https://noncepad.com/dev/nightly/macos/arm/solpipe.tar.gz"
-      sha256 "30eb9636e11253c809350a4f6c3814d64261f2ed768ef9d9dca38ac33c1ecbb0"
+      sha256 "78b2ed24c2d79580478fae0c41a741a1db1b165ffe3c39e9421bebf0100e0240"
     end
   end
     
